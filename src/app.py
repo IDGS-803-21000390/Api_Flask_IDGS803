dca221f69a990dbf40c424d23cf452d0dc10d7ef
IDGS-803-21000390 <156369037+IDGS-803-21000390@users.noreply.github.com>
2024-03-01 18:54:44 -0600

    get YPH 01/03/2024

---
+++ src/app.py
@@ -0,0 +1,30 @@
+from flask import Flask,request,jsonify
+from flask_mysql import MySQL
+from config import config
+
+
+app=Flask(__name__)
+
+con=MySQL(app)
+
+
+
+@app.route('/alumnos', methods=['GET'])
+def listar_alumnos():
+    try:
+        cursor=con.connection.cursor()
+        sql='select * from alumnos'
+        cursor.excecute(sql)
+        datos=cursor.fetchall()
+        alumnos=[]
+        for fila in datos:
+            alumno={'matricula':fila[0],'nombre':fila[1],'apaterno':fila[2],'amaterno':fila[3],'correo':fila[4]}
+            alumnos.append(alumno)
+            print(alumnos)
+        return jsonify({'alumnos':alumnos,'mensaje':'Lista de alumnos','exito':True})
+    except Exception as ex:
+        return jsonify({'mensaje':'error {}'.format(ex), 'exito':False})
+
+
+
+
---
+++ src/config.py
@@ -0,0 +1,8 @@
+class DevelopmentConfig():
+    DEBUG=True
+    MYSQL_HOST='localhost'
+    MYSQL_USER='yaz'
+    MYSQL_PASSWORD='12345678'
+    MYSQL_DB='api_utl_idgs803'
+
+config={'develoment':DevelopmentConfig}
